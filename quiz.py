1fb78254f2e553caae0dca7927c03737496bf7ca
BehnazSdt <behnazsdt11111@gmail.com>
2026-07-18 02:23:14 -0700

    Implement grade_ message method
    Overrode the grade _message method to provide quiz _specific feedback based on the student's score.

--- quiz.py
+++ quiz.py
@@ -3,3 +3,13 @@ from assessment import Assessment
 class Quiz(Assessment):
     def display_info(self):
         print(f"Quiz{self.title}- Max score: {self.max_score}")
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 80:
+            return "Great quiz result."
+        else:
+            return "Needs more practice."
+
+
