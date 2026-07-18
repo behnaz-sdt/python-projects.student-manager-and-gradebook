1a2348206e904400d587dcc888f2bdada2aeeb3b
BehnazSdt <behnazsdt11111@gmail.com>
2026-07-18 05:01:44 -0700

    Add project grade message
    Override grade_ message for project feedback.

--- project.py
+++ project.py
@@ -3,3 +3,11 @@ from assessment import Assessment
 class Project(Assessment):
     def display_info(self):
         print(f"Project: {self.title}- Max score: {self.max_score}")
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+
+        if percentage >= 60:
+            return "Project completed successfully."
+        else:
+            return "Project needs improvement."
